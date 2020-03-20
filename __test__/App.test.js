b81914ddf456c11fed98b4f22d53534beb350672
rlwong2 <rlwong2@gmail.com>
2020-03-20 14:34:19 -0700

    Working on tests

---
+++ __test__/App.test.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import App from '../client/src/App.jsx';
+import renderer from 'react-test-renderer';
+// import { render } from '@testing-library/react';
+
+import { configure, shallow, mount, render } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+configure({ adapter: new Adapter() });
+
+
+describe('Test to check Jest', () => {
+  it('1 plus 1 equals 2', () => {
+    expect(1 + 1).toBe(2);
+  });
+});
+
+describe('Renders components in DOM', () => {
+  test('Renders comments', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find(App)).to.have.lengthOf(1);
+  })
+})
