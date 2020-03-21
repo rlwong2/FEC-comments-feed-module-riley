4d1b9ef64ed6300e67a65297eb7085e89ac95393
rlwong2 <rlwong2@gmail.com>
2020-03-21 13:22:08 -0700

    Fix song description font-weight

--- client/src/components/Song.jsx
+++ client/src/components/Song.jsx
@@ -4,16 +4,21 @@ import styled from 'styled-components';
 const Description = styled.div`
   font-size: 14px;
   font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;
+  font-weight: 100;
   line-height: 1.3em;
+`
+
+const Bold = styled.p`
+  font-weight: 500;
 `
 
 const Song = (props) => (
   <Description data-testid="description">
-    <b>Release date: </b><br />
-    {props.song.release_date}<br />
+    <Bold>Release date: </Bold>
+    {props.song.release_date}
     <br />
-    <b>C-line: </b><br />
-    {props.song.c_line}<br />
+    <Bold>C-line: </Bold>
+    {props.song.c_line}
   </Description>
 )
 
