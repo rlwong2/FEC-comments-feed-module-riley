ebbb82168bc70d3c8c9005374f590813476dbb66
rlwong2 <rlwong2@gmail.com>
2020-03-27 08:24:14 -0700

    Added AWS elastic beanstalk config

--- server/index.js
+++ server/index.js
@@ -4,18 +4,22 @@ const bodyParser = require('body-parser');
 const path = require('path');
 const $ = require('jquery');
 const router = require('./router.js');
+const cors = require('cors')
+
 
 let app = express();
+
+app.use(cors());
 
 // Serve up static html
 app.use(express.static(path.join(__dirname, '../client/dist')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  next();
-});
+// app.use((req, res, next) => {
+//   res.header('Access-Control-Allow-Origin', '*');
+//   next();
+// });
 
 // Router
 app.use('/', router);
